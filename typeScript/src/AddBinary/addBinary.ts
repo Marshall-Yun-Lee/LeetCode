606b0473b33f863bc07aac3bc70dce47a19aaf8a
Marshall <gkslql456@gmail.com>
2020-05-06 10:00:13 -0700

    stuck with climbingStairs

--- typeScript/src/AddBinary/addBinary.ts
+++ typeScript/src/AddBinary/addBinary.ts
@@ -1,4 +1,3 @@
-
 export function addBinary(first: string, second: string): string {
     let out: string = "";
     let i: number = first.length - 1;
@@ -6,9 +5,9 @@ export function addBinary(first: string, second: string): string {
     let extra: number = 0;
     while (i >= 0 || j >= 0 || extra > 0) {
         // add up as if regular integers
-        extra += (i >= 0) ? parseInt(first.charAt(i--)) : 0;
-        extra += (j >= 0) ? parseInt(second.charAt(j--)) : 0;
-        
+        extra += i >= 0 ? parseInt(first.charAt(i--)) : 0;
+        extra += j >= 0 ? parseInt(second.charAt(j--)) : 0;
+
         // convert extras to binary
         out = String(extra % 2) + out;
         extra = Math.floor(extra / 2); // TypeScript does not have integer type -- only number!! -- so flooring any decimal places (in case that extra === 1)
--- typeScript/src/climbingStairs/climbingStairs.ts
+++ typeScript/src/climbingStairs/climbingStairs.ts
@@ -1,12 +1,34 @@
 export const climbingStairs = (input: number): number => {
-    if (input === 0 || input === null) {
-        return 0;
-    } else if (input === 1) {
+    if (input === 1) {
         return 1;
     }
+    if (input === 2) {
+        return 2;
+    }
 
-    // the number itself + 1s
-    // use fibonacci?
-    let out = 0;
-    return out;
-}
+    let one: number = 1;
+    let two: number = 2;
+    for (let i: number = 3; i <= input; i--) {
+        let temp: number = two;
+        two = one + two;
+        one = temp;
+    }
+    return two;
+    // let memorize: number[] = [];
+    // const counter = (remains: number, saved: number[]) => {
+    //     if (remains < 0 || remains === null) {
+    //         return 0;
+    //     } else if (remains === 0) {
+    //         return 1;
+    //     }
+
+    //     if (saved[remains]) {
+    //         return saved[remains];
+    //     }
+    //     saved[remains] =
+    //         counter(remains - 1, saved) + counter(remains - 2, saved);
+
+    //     return saved[remains];
+    // };
+    // return counter(input, memorize);
+};
